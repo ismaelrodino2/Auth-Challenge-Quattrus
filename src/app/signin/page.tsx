1694432b0bdbe751de776ac4d98bb0144389a4b9
Ismael Rodino <ismael.rodino@hotmail.com>
2024-02-09 17:13:16 -0300

    add signin page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,7 +1,42 @@
-export default function SignIn(){
-    return(
-        <main>
-            
-        </main>
-    )
+"use client";
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  company: string;
+  username: string;
+  password: string;
+};
+
+export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="company"
+          {...register("company", { required: true })}
+        />
+
+        <input
+          placeholder="username"
+          {...register("username", { required: true })}
+        />
+
+        <input
+          placeholder="password"
+          {...register("password", { required: true })}
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
+    </main>
+  );
 }
