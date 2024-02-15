7bb3cae71bc76828f598a44129aba5891ec42791
Carol Ferraz <fferraz.carol@gmail.com>
2024-02-15 13:22:38 -0300

    feat: add bg image and align login container

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,8 +5,10 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useSignIn } from "./hooks/useSignIn";
 import { useState } from "react";
-import { Button, Typography, FormControl, TextField } from "@mui/material";
+import { Button, Typography, FormControl, TextField, Box } from "@mui/material";
 import { error } from "console";
+import logo from "./../../../assets/logo.png";
+import logoSimplify from "./../../../public/assets/logoSimplify.svg";
 
 export default function SignIn() {
   const [loading, setLoading] = useState(false);
@@ -38,92 +40,115 @@ export default function SignIn() {
 
   return (
     <>
-      <Typography
-        color="primary"
-        align="left"
-        gutterBottom={true}
-        variant="inherit"
-        sx={{ fontWeight: "bold" }}
+      <div
+        style={{
+          backgroundImage: `url(${logoSimplify.src})`,
+          backgroundSize: "contain",
+          backgroundPosition: "right",
+          backgroundRepeat: "no-repeat",
+          height: "100vh",
+          overflow: "hidden",
+        }}
       >
-        Seja bem-vindo!
-      </Typography>
-      <FormControl onSubmit={handleSubmit(onSubmit)}>
-        <TextField
+        <Box
           sx={{
-            borderRadius: "8px",
-            color: "primary.main",
-            fontSize: "12px",
-            paddingTop: 0,
-            paddingBottom: "2px",
+            borderRadius: "20px",
+            background: "linear-gradient(315deg, background.default, grey.100)",
+            boxShadow: "-19px -19px 38px #bebebe, 19px 19px 38px #ffffff",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center', // Vertically center the content
+            position: 'fixed',
+            left: '20%',
+            top: '50%', 
+            transform: 'translateY(-50%)',
+            padding: '30px',
+            maxWidth: '100%'
           }}
-          variant="standard"
-          color="primary"
-          label="Empresa"
-          {...register("company", { required: true })}
-        />
-        {errors?.company && (
-          <Typography color="error">
-            {errors.company.message}
-          </Typography>
-        )}
-        <TextField
-          sx={{
-            color: "primary.main",
-            fontSize: "12px",
-            paddingTop: 0,
-            paddingBottom: "2px",
-            // "& .MuiInput-input": {
-            //   backgroundColor: "grey.100",
-            //   borderRadius: "4px",
-            // },
-          }}
-          variant="standard"
-          color="primary"
-          label="Nome"
-          {...register("username", { required: true })}
-        />
-        {errors?.username && (
-          <Typography color="error">
-            {errors.username.message}
-          </Typography>
-        )}
-        <TextField
-          sx={{
-            borderRadius: "8px",
-            color: "primary.main",
-            fontSize: "12px",
-            paddingTop: 0,
-            paddingBottom: "2px",
-          }}
-          variant="standard"
-          color="primary"
-          label="Senha"
-          {...register("password", { required: true })}
-        />
-        {errors?.password && (
-          <Typography color="error">
-            {errors.password.message}
-          </Typography>
-        )}
-        <Button
-          sx={{
-            borderRadius: "8px",
-            backgroundColor: "primary.main",
-            color: "common.white",
-            padding: "13px 10px",
-            fontSize: "12px",
-            maxWidth: "115px",
-            "&:hover": {
-              color: "common.black",
-              borderColor: "grey.400",
-              border: "1px",
-            },
-          }}
-          type="submit"
         >
-          Enviar
-        </Button>
-      </FormControl>
+         
+          <Typography
+            color="primary"
+            align="left"
+            gutterBottom={true}
+            variant="inherit"
+            sx={{ fontWeight: "bold" }}
+          >
+            Seja bem-vindo!
+          </Typography>
+          <FormControl onSubmit={handleSubmit(onSubmit)} sx={{ gap: '10px' }}>
+            <TextField
+              sx={{
+                borderRadius: "8px",
+                color: "primary.main",
+                fontSize: "12px",
+                paddingTop: 0,
+                paddingBottom: "2px",
+              }}
+              variant="standard"
+              color="primary"
+              label="Empresa"
+              {...register("company", { required: true })}
+            />
+            {errors?.company && (
+              <Typography color="error">{errors.company.message}</Typography>
+            )}
+            <TextField
+              sx={{
+                color: "primary.main",
+                fontSize: "12px",
+                paddingTop: 0,
+                paddingBottom: "2px",
+                // "& .MuiInput-input": {
+                //   backgroundColor: "grey.100",
+                //   borderRadius: "4px",
+                // },
+              }}
+              variant="standard"
+              color="primary"
+              label="Nome"
+              {...register("username", { required: true })}
+            />
+            {errors?.username && (
+              <Typography color="error">{errors.username.message}</Typography>
+            )}
+            <TextField
+              sx={{
+                borderRadius: "8px",
+                color: "primary.main",
+                fontSize: "12px",
+                paddingTop: 0,
+                paddingBottom: "2px",
+              }}
+              variant="standard"
+              color="primary"
+              label="Senha"
+              {...register("password", { required: true })}
+            />
+            {errors?.password && (
+              <Typography color="error">{errors.password.message}</Typography>
+            )}
+            <Button
+              sx={{
+                borderRadius: "8px",
+                backgroundColor: "primary.main",
+                color: "common.white",
+                padding: "7px 10px",
+                fontSize: "12px",
+                maxWidth: "115px",
+                "&:hover": {
+                  color: "common.black",
+                  borderColor: "grey.400",
+                  border: "1px",
+                },
+              }}
+              type="submit"
+            >
+              Entrar
+            </Button>
+          </FormControl>
+        </Box>
+      </div>
     </>
   );
 }
