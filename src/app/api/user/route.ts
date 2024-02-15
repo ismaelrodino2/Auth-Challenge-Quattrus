018d56f76f0623baac0089885785d5c8c6534586
Ismael Rodino <ismael.rodino@hotmail.com>
2024-02-15 15:41:10 -0300

    add hash password

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,41 +3,52 @@ import { createUserFormSchema } from "@/lib/schemas";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import * as jose from "jose";
+import * as bcrypt from "bcrypt";
 
 export async function POST(req: Request) {
   try {
-    const body = await req.json();
-    const { username, password, company } = createUserFormSchema.parse(body);
+    const body = createUserFormSchema.parse(await req.json());
+    const { username, company } = createUserFormSchema.parse(body);
 
     const user = await prisma.user.findUnique({
       where: {
         username,
         company,
-        password,
       },
     });
-
-    //token
-    if (user) {
-      const { password, ...userWithoutPassword } = user;
-
-      const token = await new jose.SignJWT(userWithoutPassword)
-        .setProtectedHeader({ alg: "HS256", typ: "JWT" })
-        .sign(new TextEncoder().encode(process.env.JWT_SECRET));
-
-      //set cookie as token
-      cookies().set({
-        name: "auth",
-        value: token,
-        httpOnly: true,
-        path: "/",
-      });
-      return NextResponse.json({ user }, { status: 200 });
+    if (!user) {
+      return NextResponse.json({ message: "User not found." }, { status: 404 });
     }
+    //token
+    const match = await bcrypt.compare(body.password, user?.password);
+
+    if (!match) {
+      return NextResponse.json(
+        { message: "Invalid password." },
+        { status: 401 }
+      );
+    }
+
+    console.log("As senhas correspondem.");
+
+    const { password, ...userWithoutPassword } = user;
+
+    const token = await new jose.SignJWT(userWithoutPassword)
+      .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+      .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+
+    //set cookie as token
+    cookies().set({
+      name: "auth",
+      value: token,
+      httpOnly: true,
+      path: "/",
+    });
+    return NextResponse.json({ user }, { status: 200 });
   } catch (err) {
     console.error(err);
     return NextResponse.json(
-      { message: "Something went wrong!" },
+      { message: "An error occurred." },
       { status: 500 }
     );
   } finally {
