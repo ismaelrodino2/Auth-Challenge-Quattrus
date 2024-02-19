3d5c355dc74993244d9f4dac42a5e9f4b4bb4bfe
Ismael Rodino <ismael.rodino@hotmail.com>
2024-02-19 11:00:34 -0300

    add logout

--- src/app/components/navbarTop/navBarTop.tsx
+++ src/app/components/navbarTop/navBarTop.tsx
@@ -1,8 +1,18 @@
-"use client";;
-import { Avatar, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography } from "@mui/material";
+"use client";
+import {
+  Avatar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import logoQuattrus from "./../../../../public/assets/logoQuattrusWhite.svg";
 import logoProject from "./../../../../public/assets/logoProject.svg";
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
 
 const TopBar: React.FC = () => {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
@@ -11,10 +21,15 @@ const TopBar: React.FC = () => {
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   );
-  const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
+  const settings = ["Profile", "Account", "Dashboard"];
+  const router = useRouter();
   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
+  };
+
+  const handleSignOut = () => {
+    deleteCookie("auth", { path: "/path", domain: ".yourdomain.com" });
+    router.push("/");
   };
 
   const handleCloseUserMenu = () => {
@@ -24,7 +39,10 @@ const TopBar: React.FC = () => {
   return (
     <div className="bg-primaryMain flex items-center">
       <div className="h-full items-center justify-center bg-project">
-        <img className="h-full md:py-[21px] md:px-[16.4px] py-[22px] px-[12.4px]" src={logoProject.src}></img>
+        <img
+          className="h-full md:py-[21px] md:px-[16.4px] py-[22px] px-[12.4px]"
+          src={logoProject.src}
+        ></img>
       </div>
       <div className="flex flex-1 items-center h-12">
         <img className="max-h-4 me-2 ms-4" src={logoQuattrus.src}></img>
@@ -33,33 +51,36 @@ const TopBar: React.FC = () => {
         </Typography>
       </div>
       <Toolbar>
-          <Tooltip title="Abrir configurações">
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: "45px" }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: "top",
-              horizontal: "right",
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
+        <Tooltip title="Abrir configurações">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          <MenuItem onClick={handleCloseUserMenu}>
+            <button onClick={handleSignOut}>Logout</button>
             {settings.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">{setting}</Typography>
-              </MenuItem>
+              <Typography key={setting} textAlign="center">
+                {setting}
+              </Typography>
             ))}
-          </Menu>
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </div>
   );
